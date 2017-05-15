3a97fcca896b373cb15347cef583585f64f604fb
Munkittaja <joni.kurki@gmail.com>
2017-05-15 15:33:05 +0300

    finished tutorial @https://www.youtube.com/watch?v=AM7wBz9azyU&list=PLbghT7MmckI42Gkp2cILkO2nRxK2M4NLo&index=3

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform originalParent = null;
+	public Transform parentPlaceholder = null;
+
+	public GameObject cardPlaceholder = null;
+
+	public void OnBeginDrag( PointerEventData eventData){
+		Debug.Log ("OnBeginDrag");
+
+		cardPlaceholder = new GameObject ();
+		cardPlaceholder.transform.SetParent(this.transform.parent);
+		LayoutElement le = cardPlaceholder.AddComponent<LayoutElement> ();
+		le.preferredWidth = this.GetComponent<LayoutElement> ().preferredWidth;
+		le.preferredHeight = this.GetComponent<LayoutElement> ().preferredHeight;
+		le.flexibleWidth = 0;
+		le.flexibleHeight = 0;
+
+		cardPlaceholder.transform.SetSiblingIndex (this.transform.GetSiblingIndex());
+
+		originalParent = this.transform.parent;
+		parentPlaceholder = originalParent;
+		this.transform.SetParent (this.transform.parent.parent);
+
+		this.GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData){
+
+		this.transform.position = eventData.position;
+		int newSiblingIndex = parentPlaceholder.childCount;
+		if (cardPlaceholder.transform.parent = parentPlaceholder)
+			cardPlaceholder.transform.parent.SetParent (parentPlaceholder);
+		for (int i=0; i < parentPlaceholder.childCount; i++) {
+			if (this.transform.position.x < parentPlaceholder.GetChild (i).position.x) {
+
+				newSiblingIndex = i;
+				if (parentPlaceholder.transform.GetSiblingIndex () < newSiblingIndex)
+					newSiblingIndex--;
+
+
+				break;
+			}
+		}
+		cardPlaceholder.transform.SetSiblingIndex (newSiblingIndex);
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		Debug.Log ("OnEndDrag");
+		this.transform.SetParent(originalParent);
+		this.transform.SetSiblingIndex (cardPlaceholder.transform.GetSiblingIndex ());
+		this.GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		Destroy(cardPlaceholder);
+	}
+}
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+
+	public void OnPointerEnter(PointerEventData eventData){
+		if (eventData.pointerDrag == null)
+			return;
+
+		Draggable drag = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (drag != null) {
+			drag.parentPlaceholder = this.transform;
+		}
+	}
+	public void OnPointerExit(PointerEventData eventData){
+		if (eventData.pointerDrag == null)
+			return;
+		
+		Draggable drag = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (drag != null && drag.parentPlaceholder == this.transform) {
+			drag.parentPlaceholder = drag.originalParent;
+		}
+	}
+	public void OnDrop(PointerEventData eventData){
+		Debug.Log (eventData.pointerDrag.name +" was dropped OnDrop to "+gameObject.name);
+
+		Draggable drag = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (drag != null) {
+			drag.originalParent = this.transform;
+		}
+	}
+}
